85a1441c702be154d65b44e1c9e0fd2413330994
Lucas Reinoso <lucas.reinoso.br@gmail.com>
2021-02-11 18:20:01 -0300

    Window

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame,sys
+from pygame.locals import *
+
+pygame.init()
+
+WINDOW_SIZE = (800,640)
+
+window = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Platformer Jump")
+
+loop = True
+
+while loop:
+    window.fill((135, 206, 235))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
