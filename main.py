4d3fd0b882b0fb22378a89c5f41a4c3bb16e6f33
Lucas Reinoso <lucas.reinoso.br@gmail.com>
2021-02-11 19:31:11 -0300

    Player Move

--- main.py
+++ main.py
@@ -5,21 +5,34 @@ from player import Player
 pygame.init()
 
 WINDOW_SIZE = (640,800)
+DISPLAY_SIZE = (160,200)
 
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Platformer Jump")
 
+display = pygame.Surface(DISPLAY_SIZE)
+
+
+time = pygame.time.Clock()
 loop = True
 
-player = Player(100,100)
+player = Player(10,10)
 
 while loop:
-    window.fill((135, 206, 235))
+    display.fill((135, 206, 235))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
-    player.draw(window)
+    
+        player.move_dir(event)
+        
+    #draw
+    player.draw(display)
+
+    #update
+    player.update()
+    window.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
     pygame.display.update()
+    time.tick(60)
--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 import pygame
 pygame.init()
 
+GRAVITY = 0.1
+
 class Player:
     def __init__(self,x,y):
 
@@ -9,11 +11,45 @@ class Player:
                     pygame.image.load("assets/run_2.png")]
         
         self.rect = self.img[0].get_rect()
-        self.rect[0] = x
-        self.rect[1] = y
+        self.rect.x = x
+        self.rect.y = y
+        self.y_momentum = 0
+        self.x_vel = 2
+
+        self.jump_force = 3
+
+        self.right = False
+        self.left = False
 
     def draw(self,display):
-        display.blit(self.img[0],(self.rect[0],self.rect[1]))
+        display.blit(self.img[0],(self.rect.x,self.rect.y))
 
-    def update():
-        pass
+    def update(self):
+        self.gravity()
+        self.move()
+
+    def gravity(self):
+        self.y_momentum += GRAVITY
+        self.rect.y += self.y_momentum
+
+    def jump(self):
+        self.y_momentum = -self.jump_force
+    
+    def move_dir(self,event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.right = True
+            if event.key == pygame.K_LEFT:
+                self.left = True
+            if event.key == pygame.K_SPACE:
+                self.jump()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.right = False
+            if event.key == pygame.K_LEFT:
+                self.left = False
+    def move(self):
+        if self.right:
+            self.rect.x += self.x_vel
+        if self.left:
+            self.rect.x -= self.x_vel
